90b3b73a3435343492da58154bc584712a6cbb96
archer2018 <617948175@qq.com>
2020-02-23 00:44:04 +0800

    '增加单例模式'

---
+++ src/construct/singleton/hungry/HungrySingleton.java
@@ -0,0 +1,10 @@
+package construct.singleton.hungry;
+
+/**
+ * @Author: Archer
+ * @Date: 2020/2/22
+ * @Description:
+ * @Version: 1.0
+ */
+public class HungrySingleton {
+}
---
+++ src/construct/singleton/lazy/staticinnerclass/InnerClassSingleton.java
@@ -0,0 +1,10 @@
+package construct.singleton.lazy.staticinnerclass;
+
+/**
+ * @Author: Archer
+ * @Date: 2020/2/23
+ * @Description:
+ * @Version: 1.0
+ */
+public class InnerClassSingleton {
+}
